21e1c1c123057c92f1fa2180aad6874fed0d70ce
吴灏 <wuh@cnlemon.net>
2021-07-19 17:57:22 +0800

    feat:安装nodemon以便热更新

--- src/app.js
+++ src/app.js
@@ -2,13 +2,14 @@
  * @Author: 吴灏
  * @Date: 2021-07-17 17:07:54
  * @LastEditors: 吴灏
- * @LastEditTime: 2021-07-17 17:14:20
+ * @LastEditTime: 2021-07-19 17:49:42
  * @Description: file content
  */
+// #region nodejs内置os库
 // 读取系统cpu信息
 const os = require("os");
-
-const cpus = os.cpus(); // 获取当前系统的cpu的数量
+// 获取当前系统的cpu的数量
+const cpus = os.cpus();
 
 console.log(cpus.length);
 
@@ -21,10 +22,12 @@ console.log(total / 1024 / 1024 / 1024);
 const rest = os.freemem();
 
 console.log(rest / 1024 / 1024 / 1024);
+// #endregion
 
+// #region nodejs内置http库
 // web服务
 const http = require("http");
-
+// 创建web服务对象
 const server = http.createServer((req, res) => {
   res.end("hello");
 });
@@ -32,3 +35,4 @@ const server = http.createServer((req, res) => {
 server.listen(8000, "127.0.0.1", () => {
   console.log("服务启动成功");
 });
+// #endregion
